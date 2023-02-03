2507b3d97e9d8d834dc5701c62c48ff597a69c4d
Miray <sonmezmiray@gmail.com>
2023-02-04 01:50:21 +0300

    Doc: add basic components and routes

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import About from './containers/About';
+import Experience from './containers/Experience';
+import Home from './containers/Home';
+import Projects from './containers/Projects';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './containers/App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
