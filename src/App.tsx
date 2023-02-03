0df980b2e9314523adf012ea815ce0e6406ec820
Miray <sonmezmiray@gmail.com>
2023-02-03 18:03:18 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Miray Sönmez</h1>
+    </div>
+  );
+}
+
+export default App;
