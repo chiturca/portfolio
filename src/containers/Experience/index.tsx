9bc77e4e0bbc2c1738ab478331fa6f602d6c9a22
Miray <sonmezmiray@gmail.com>
2023-02-05 03:26:47 +0300

    Refactor: add experiences

--- src/containers/Experience/index.tsx
+++ src/containers/Experience/index.tsx
@@ -1,7 +1,94 @@
 import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="blueviolet">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="September 2022 - February 2023"
+          iconStyle={{ background: 'blueviolet', color: '#fff' }}>
+          <h3 className="vertical-timeline-element-title">Re:Coded Bootcamp</h3>
+          <h4 className="vertical-timeline-element-subtitle">Web Development</h4>
+          <p>Frontend Web Developer</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="March 2019 - September 2020"
+          iconStyle={{ background: 'blueviolet', color: '#fff' }}>
+          <h3 className="vertical-timeline-element-title">Turkcell Global Bilgi</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Customer Support Representative - Full Time
+          </h4>
+          <p>
+            Handling customer feedbacks about their trip procedure from the time they reserved their
+            tickets to the end of their travel process with Turkish Airlines.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="September 2018 - March 2019"
+          iconStyle={{ background: 'blueviolet', color: '#fff' }}>
+          <h3 className="vertical-timeline-element-title">Turkcell Global Bilgi - Full Time</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Inbound Customer Service Representative
+          </h4>
+          <p>Inbound calls on behalf of Turkish Airlines</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="April 2018 - July 2018"
+          iconStyle={{ background: 'blueviolet', color: '#fff' }}>
+          <h3 className="vertical-timeline-element-title">Tea Co. - Full Time</h3>
+          <h4 className="vertical-timeline-element-subtitle">Barista</h4>
+          <p>Present international tea brands</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="April 2015 - June 2016"
+          iconStyle={{ background: 'blueviolet', color: '#fff' }}>
+          <h3 className="vertical-timeline-element-title">RS Plan-Project - Seasonal</h3>
+          <h4 className="vertical-timeline-element-subtitle">Project Manager</h4>
+          <p>Erasmus+ Project Management</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="March 2014 - Present"
+          iconStyle={{ background: 'blueviolet', color: '#fff' }}>
+          <h3 className="vertical-timeline-element-title">
+            Ankara Youth Philharmonic Orchestra/Polyphonic Choir - AGFO
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Musician- Vice Coordinator- Volunteer
+          </h4>
+          <p>Polyphonic choir member, vice coordinator and volunteer</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="September 2013 - March 2014"
+          iconStyle={{ background: 'blueviolet', color: '#fff' }}>
+          <h3 className="vertical-timeline-element-title">
+            Nicolaus Copernicus University-Poland Torun
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">Erasmus exchange student</h4>
+          <p>Political Sciences</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="September 2010 - June 2016"
+          iconStyle={{ background: 'blueviolet', color: '#fff' }}>
+          <h3 className="vertical-timeline-element-title">Ankara University</h3>
+          <h4 className="vertical-timeline-element-subtitle">Bachelor of Arts -Sociology</h4>
+          <p>
+            Undergraduate thesis: Crime Sociology on Çinçin and Yenidoğan neighbourhood : Alteration
+            on crime due to urban renewal
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
