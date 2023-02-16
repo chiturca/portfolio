3684157bcb74b29c5aa48e05e1d0b69df1df6543
Miray <sonmezmiray@gmail.com>
2023-02-16 22:05:14 +0300

    Refactor: add footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './index.scss';
+import CV from '../../assets/footer/Miray_Sönmez.pdf';
 
 function Footer() {
   return (
     <div className="footer">
       <div className="contact">
-        <button className="cv">download</button>
-        <button className="email">e</button>
-        <button className="github">g</button>
-        <button className="linkedin">l</button>
+        <button className="cv">
+          <a href={CV} download>
+            download CV
+          </a>
+        </button>
+        <button className="email">
+          <a href={'mailto:sonmezmiray@gmail.com'} target="_blank" rel="noreferrer">
+            Email
+          </a>
+        </button>
+        <button className="github">
+          <a href={'https://github.com/chiturca'} target="_blank" rel="noreferrer">
+            Github
+          </a>
+        </button>
+        <button className="linkedin">
+          <a href={'https://www.linkedin.com/in/miraysonmez'} target="_blank" rel="noreferrer">
+            Likedin
+          </a>
+        </button>
       </div>
       <p> &copy; YEAR FUNCTION 2023 miraysonmez.vercel.app</p>
     </div>
---
+++ src/env.d.ts
@@ -0,0 +1,20 @@
+declare module '*.svg' {
+  const src: string;
+  export const ReactComponent: unknown;
+  export default src;
+}
+declare module '*.pdf' {
+  const src: string;
+  export const ReactComponent: unknown;
+  export default src;
+}
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+type CSSModuleClasses = { readonly [key: string]: string };
+
+declare module '*.module.scss' {
+  const classes: CSSModuleClasses;
+  export default classes;
+}
