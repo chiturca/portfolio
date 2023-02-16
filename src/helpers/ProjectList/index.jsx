f658fe7b03aa4c0af02851b801e26a1b7f2a7762
Miray <sonmezmiray@gmail.com>
2023-02-16 12:12:51 +0300

    Doc: update projectlist

--- src/helpers/ProjectList/index.jsx
+++ src/helpers/ProjectList/index.jsx
@@ -1,10 +1,22 @@
 import refubook from '../../assets/projects/refubook.png';
+import madlibs from '../../assets/projects/madlibs.png';
 
 export const ProjectList = [
   {
-    name: 'The Incredibles Refubook',
+    name: 'The Incredibles - Refubook',
     image: refubook,
     skills: 'React.js, Redux, Firebase, TailwindCSS',
-    summary: 'A platform where every refugee can interact with and create content'
+    summary:
+      'A platform where every refugee can interact with and create content, created with team',
+    github: 'https://github.com/Recoded-Spark-Turkey-2022/The-Incredibles',
+    website: 'https://the-incredibles-refubook.netlify.app/'
+  },
+  {
+    name: 'Runtime Terror - MadLibs',
+    image: madlibs,
+    skills: 'JavaScript, CSS, HTML',
+    summary: 'A single page web game platform based on filling the blanks, created with team',
+    github: 'https://github.com/Recoded-Spark-Turkey-2022/mad-libs-runtime-terror',
+    website: 'https://spookymadlibs.netlify.app/'
   }
 ];
