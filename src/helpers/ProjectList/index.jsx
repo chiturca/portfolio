b41a7564b67496beb48a006313c0d4a9c34da8f9
Miray <sonmezmiray@gmail.com>
2023-02-18 17:50:34 +0300

    Style: fix projects position

--- src/helpers/ProjectList/index.jsx
+++ src/helpers/ProjectList/index.jsx
@@ -7,7 +7,7 @@ export const ProjectList = [
     image: refubook,
     skills: 'React.js, Redux, Firebase, TailwindCSS',
     summary:
-      'A platform where every refugee can interact with and create content, created with team',
+      'A platform where every refugee can interact with and create content (created with a team)',
     github: 'https://github.com/Recoded-Spark-Turkey-2022/The-Incredibles',
     website: 'https://the-incredibles-refubook.netlify.app/'
   },
@@ -15,7 +15,7 @@ export const ProjectList = [
     name: 'Runtime Terror - MadLibs',
     image: madlibs,
     skills: 'JavaScript, CSS, HTML',
-    summary: 'A single page web game platform based on filling the blanks, created with team',
+    summary: 'A single page web game platform based on filling the blanks (created with a team)',
     github: 'https://github.com/Recoded-Spark-Turkey-2022/mad-libs-runtime-terror',
     website: 'https://spookymadlibs.netlify.app/'
   }
