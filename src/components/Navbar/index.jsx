fe1c05e9a83b120c6eda85ac87632a5145076264
Miray <sonmezmiray@gmail.com>
2023-02-20 17:35:30 +0300

    Style: change font and fix some scaling

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,36 +1,24 @@
-import { useState, useEffect } from 'react';
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Logo from '../../assets/navbar/logo.svg';
-// import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
 import Face3Icon from '@mui/icons-material/Face3';
 import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
 import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import CV from '../../assets/footer/Miray_Sönmez.pdf';
+import DownloadIcon from '@mui/icons-material/Download';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import './index.scss';
 
 function Navbar() {
-  const [expand, setExpand] = useState(false);
-  const location = useLocation();
-
-  useEffect(() => {
-    setExpand(false);
-  }, [location]);
-
   return (
-    <div className="navbar" id={expand ? 'open' : 'close'}>
+    <div className="navbar">
       <div className="logo">
         <Link to="/">
           <img src={Logo} alt="logo" />
         </Link>
       </div>
-      {/* <div className="toggleButton">
-        <button
-          onClick={() => {
-            setExpand((prev) => !prev);
-          }}>
-          <MenuIcon />
-        </button>
-      </div> */}
       <div className="links">
         <NavLink exact="true" activeclassname="active" to="/">
           <HomeIcon />
@@ -44,6 +32,20 @@ function Navbar() {
         <NavLink exact="true" activeclassname="active" to="/experience">
           <WorkHistoryIcon />
         </NavLink>
+      </div>
+      <div className="smmedia">
+        <a href={CV} download className="cv">
+          <DownloadIcon />
+        </a>
+        <a href={'mailto:sonmezmiray@gmail.com'} target="_blank" rel="noreferrer">
+          <EmailIcon />
+        </a>
+        <a href={'https://github.com/chiturca'} target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
+        <a href={'https://www.linkedin.com/in/miraysonmez'} target="_blank" rel="noreferrer">
+          <LinkedInIcon />
+        </a>
       </div>
     </div>
   );
