8cf25d09189f980567e533063a6488f8ce6c53c5
Miray <sonmezmiray@gmail.com>
2023-02-20 18:30:29 +0300

    Fix: projectList cards

--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -18,7 +18,7 @@ function Projects() {
       <h1 className="projectTitle">
         <AnimatedLetters letterClass={letterClass} strArray={projectsArray} idx={1} />
       </h1>
-      <div className="projectList animate__animated animate__jackInTheBox">
+      <div className="projectList animate__animated animate__pulse">
         {ProjectList.map((project, index) => {
           return <ProjectItem id={index} key={index} name={project.name} image={project.image} />;
         })}
