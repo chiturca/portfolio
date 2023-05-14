e767eceafd45cff783624c87138dd79bd91f40b0
Miray Sonmez <sonmezmiray@gmail.com>
2023-05-14 18:44:35 +0300

    Feat: add popover for certificates on experience

--- src/containers/Experience/index.tsx
+++ src/containers/Experience/index.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import { ExperienceList } from '../../helpers/ExperienceList/index';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import { Popover } from '@mui/material';
 import './index.scss';
 
 function Experience() {
+  const [anchorEl, setAnchorEl] = useState<HTMLImageElement | null>(null);
+  const [imageSrc, setImageSrc] = useState('');
+
+  // eslint-disable-next-line no-undef
+  const handleClick = (event: React.MouseEvent<HTMLImageElement>) => {
+    setAnchorEl(event.currentTarget);
+    setImageSrc(event.currentTarget.src);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'popover-image' : undefined;
+
   return (
     <div className="experience">
       <VerticalTimeline lineColor="#fff">
@@ -19,12 +37,30 @@ function Experience() {
               <h3 className="vertical-timeline-element-title">{project.h3}</h3>
               <h4 className="vertical-timeline-element-subtitle">{project.h4}</h4>
               <p>{project.p}</p>
-              <img src={project.img} height="50vw" /> <img src={project.img2} height="50vw" />{' '}
-              <img src={project.img3} height="50vw" />
+              <div>
+                <img src={project.img} height="50vw" onClick={handleClick} />{' '}
+                {project.img2 && <img src={project.img2} height="50vw" onClick={handleClick} />}{' '}
+                {project.img3 && <img src={project.img3} height="50vw" onClick={handleClick} />}
+              </div>
             </VerticalTimelineElement>
           );
         })}
       </VerticalTimeline>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'center'
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'center'
+        }}>
+        <img src={imageSrc} style={{ width: '500px', height: '400px' }} />
+      </Popover>
     </div>
   );
 }
