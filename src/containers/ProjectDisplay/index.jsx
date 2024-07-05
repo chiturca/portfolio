4fd5589128e9d162f42395ddb5c5985feca407ac
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2024-07-05 11:50:01 +0300

    Refactor: update projects

--- src/containers/ProjectDisplay/index.jsx
+++ src/containers/ProjectDisplay/index.jsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
-import { FrontList, UnityList } from "../../helpers/ProjectList";
+import { FrontList, UnityList, TestList } from "../../helpers/ProjectList";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import HttpIcon from "@mui/icons-material/Http";
+import AssessmentIcon from "@mui/icons-material/Assessment";
 import "./index.scss";
 
 function ProjectDisplay() {
   const { id } = useParams();
-  const project = FrontList[id] || UnityList[id];
+  const projectId = parseInt(id, 10);
+  const project =
+    TestList.find((p) => p.id === projectId) ||
+    FrontList.find((p) => p.id === projectId) ||
+    UnityList.find((p) => p.id === projectId);
 
   return (
     <div className="project">
@@ -21,11 +26,19 @@ function ProjectDisplay() {
             {project.github !== "" && (
               <a href={project.github} target="_blank" rel="noreferrer">
                 <GitHubIcon />
+                <span>GITHUB</span>
               </a>
             )}
             {project.website !== "" && (
               <a href={project.website} target="_blank" rel="noreferrer">
                 <HttpIcon />
+                <span>WEBSITE</span>
+              </a>
+            )}
+            {project.report !== "" && (
+              <a href={project.report} target="_blank" rel="noreferrer">
+                <AssessmentIcon />
+                <span>TEST REPORTS</span>
               </a>
             )}
           </div>
